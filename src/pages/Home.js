5f36fad96a3d8964e87a05504070575f66827f67
Khai Thieu <khaithieu3@gmail.com>
2022-10-28 20:46:31 -0500

    improved home page responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,17 @@ import { Link } from 'react-router-dom';
 
 function Home()
 {
+    document.body.style.backgroundImage = "none";
+
     return (
         <>
         <nav>
             <NavBar />
         </nav>
-        <main className="flex flex-wrap m-auto xl:w-[910px] lg:w-[680px] md:w-[460px] sm:w-[230px]">
+        <main className="flex flex-wrap m-auto w-[1018px] xl:w-[815px] lg:w-[610px] md:w-[408px] sm:w-[200px]">
             {animeInfo.map((info) => {
                 return (
-                    <Link to="/info" state={{title: info.title}} className="no-underline">
+                    <Link to="/info" state={{title: info.title, image: info.img}} className="no-underline">
                         <Anime key={info.title} title={info.title} img={info.img} season={info.season} />
                     </Link>
                 )
