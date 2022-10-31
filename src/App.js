5f505514af9132b492bd79f36f57de0764b57502
Khai Thieu <khaithieu3@gmail.com>
2022-10-31 15:10:57 -0500

    use lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import AnimeInfo from './pages/AnimeInfo';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const AnimeInfo = React.lazy(() => import('./pages/AnimeInfo'));
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/info" element={<AnimeInfo />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/info" element={<AnimeInfo />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
