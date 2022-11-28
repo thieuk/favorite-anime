0363510b06c20e147519942862ab1401ed5aeb2d
Khai Thieu <khaithieu3@gmail.com>
2022-11-27 21:45:20 -0600

    minor changes to the style

--- client/src/components/AnimeOverlay.js
+++ client/src/components/AnimeOverlay.js
@@ -1,6 +1,6 @@
 export default function AnimeOverlay() {
     return (
-        <div id="overlay" className="absolute top-0 left-0 w-full h-full flex items-center justify-center rounded-xl bg-zinc-700 transition-opacity duration-500 cursor-pointer">
+        <div id="overlay" className="absolute top-0 left-0 w-full h-full flex items-center justify-center rounded-xl bg-zinc-700 transition-opacity duration-500 border-b-2 border-amber-300 cursor-pointer">
             <p className="font-bold">Click for More Info</p>
         </div>
     );
